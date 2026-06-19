59ac7eb658fb1a05a3c52def286e7eb591130360
Cretamoin <cretamoin2@gmail.com>
2026-06-19 22:01:30 +0300

    réparation coden'i Hasina

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const controller = require('../controllers/auth.controller.js');
+const {authenticate }= require('../middlewares/authenticates.js');
+const authorize = require('../middlewares/authorize.js');
+
+router.post('/register', controller.register);
+router.post('/registerSU', authorize('admin'), controller.registerSU);   
+router.post('/login', controller.login);
+router.post('/refresh', controller.refresh);
+router.get('/profile', authenticate, controller.profile);
+router.post('/logout', authenticate, controller.logout);
+
+
+
+module.exports = router;
